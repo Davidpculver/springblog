305faef09a01dd8f1e3c3ec030fc47700f43b5c6
David Culver <culver.david.p@gmail.com>
2021-07-27 00:04:49 -0500

    working on form model binding exercise

--- src/main/java/com/codeup/springblog/controllers/PostController.java
+++ src/main/java/com/codeup/springblog/controllers/PostController.java
@@ -63,60 +63,96 @@ public class PostController {
 //        return "posts/show";
 //    }
 
-    private final PostRepository postRepo;
+    private final PostRepository postDao;
     private final UserRepository userDao;
 
-    public PostController(PostRepository postRepo, UserRepository userDao) {
-        this.postRepo = postRepo;
+    public PostController(PostRepository postDao, UserRepository userDao) {
+        this.postDao = postDao;
         this.userDao = userDao;
     }
 
     @GetMapping("/posts")
     public String index(Model model) {
-        model.addAttribute("posts", postRepo.findAll());
+        model.addAttribute("posts", postDao.findAll());
         return "posts/index";
     }
 
     @GetMapping("/posts/{n}")
     public String findById(@PathVariable long n, Model model) {
-        model.addAttribute("post", postRepo.findById(n));
+        model.addAttribute("post", postDao.findById(n));
         return "posts/show";
     }
 
     @PostMapping("/posts/delete/{id}")
     public String deleteById(@PathVariable long id) {
-        postRepo.deleteById(id);
+        postDao.deleteById(id);
 //        Instructor:
 //        postRepo.delete(id);
         return "redirect:/posts";
     }
 
-    @GetMapping("/posts/edit/{id}")
+//    @GetMapping("/posts/edit/{id}")
+//    public String postToEdit(@PathVariable long id, Model model) {
+//        model.addAttribute("post", postDao.findById(id));
+//        return "posts/edit";
+//    }
+//
+//    @PostMapping("/posts/edit/update/{id}")
+//    public String editPost(@PathVariable long id, @RequestParam String title, @RequestParam String body) {
+//        Post updatedPost = postDao.getById(id);
+//        updatedPost.setTitle(title);
+//        updatedPost.setBody(body);
+//        postDao.save(updatedPost);
+//        return "redirect:/posts";
+//    }
+
+//    @GetMapping("/posts/create")
+//    public String showCreateForm() {
+//        return "posts/create";
+//    }
+
+    //    REFACTORING FOR FORM MODEL BINDING
+    @GetMapping("/posts/create")
+    public String showCreateForm(Model model) {
+        model.addAttribute("post", new Post());
+        return "posts/create";
+
+    }
+
+
+//    @PostMapping("/posts/create")
+//    public String createPost(@RequestParam String title, @RequestParam String body) {
+//        User user1 = userDao.getById(1L);
+//        Post post = new Post(title, body, user1);
+//        postRepo.save(post);
+//        return "redirect:/posts";
+//    }
+
+    //    REFACTORING FOR FORM MODEL BINDING
+    @PostMapping("/posts/create")
+    public String createPost(@ModelAttribute Post post) {
+        User user1 = userDao.getById(1L);
+        post.setUser(user1);
+        postDao.save(post);
+        return "redirect:/posts";
+    }
+
+//    REFACTORING FOR FORM MODEL BINDING
+
+    @GetMapping("/posts/{id}/edit")
     public String postToEdit(@PathVariable long id, Model model) {
-        model.addAttribute("post", postRepo.findById(id));
+        model.addAttribute("post", postDao.findById(id));
+        model.addAttribute("updatedPost", new Post());
         return "posts/edit";
     }
 
+//    Can't get edit update to work.
     @PostMapping("/posts/edit/update/{id}")
-    public String editPost(@PathVariable long id, @RequestParam String title, @RequestParam String body) {
-        Post updatedPost = postRepo.getById(id);
-        updatedPost.setTitle(title);
-        updatedPost.setBody(body);
-        postRepo.save(updatedPost);
+    public String editPost(@ModelAttribute Post post) {
+//        Post updatedPost = postDao.getById(id);
+        postDao.save(post);
         return "redirect:/posts";
     }
 
-    @GetMapping("/posts/create")
-    public String showCreateForm() {
-        return "posts/create";
-    }
-
-    @PostMapping("/posts/create")
-    public String createPost(@RequestParam String title, @RequestParam String body) {
-        User user1 = userDao.getById(1L);
-        Post post = new Post(title, body, user1);
-        postRepo.save(post);
-        return "redirect:/posts";
-    }
 
 }
